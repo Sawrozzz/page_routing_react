c48bf25a9abeb050ee9a19de1abc12b11ae4ef98
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-10-12 07:41:19 +0545

    clicking on Logout directly move you to home page

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 // import IconButton from '@mui/material/IconButton';
 // import MenuIcon from '@mui/icons-material/Menu';
-import { Link,} from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import {login, logout} from './loginSlice';
-import { useEffect } from 'react';
-import Profile from './profile';
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "./loginSlice";
+import { useEffect } from "react";
+import Profile from "./profile";
 
 export default function ButtonAppBar() {
   const userInfo = useSelector((state) => state.login);
@@ -21,10 +21,10 @@ export default function ButtonAppBar() {
     const token = localStorage.getItem("token");
     if (token) {
       dispatch(login());
-    }else{
+    } else {
       dispatch(logout());
     }
-  },[dispatch]);
+  }, [dispatch]);
   const handleLogout = () => {
     dispatch(logout());
     localStorage.removeItem("token");
@@ -32,43 +32,46 @@ export default function ButtonAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-      <Toolbar>
-        <Link to = "/" className='color'>
-        <Typography variant="h6" component="div"sx={{ flexGrow: 1 }}>
-           Namespace Cart 
-        </Typography>
-        </Link>
-        
-        <div className='nav-links'>
-          {
-          userInfo.isLoggedIn ? (
-            <>
-            <p className='login_name'> Welcome Saroj</p>
-          
-            <Button onClick={handleLogout}
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Log Out
-            
-          </Button>
-          <Link to={`/profile`}><img className='profile_img' src='https://cdn3.vectorstock.com/i/1000x1000/53/42/user-member-avatar-face-profile-icon-vector-22965342.jpg'></img>
+      <AppBar position="static">
+        <Toolbar>
+          <Link to="/" className="color">
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Namespace Cart
+            </Typography>
           </Link>
-          
-            </>
-        
 
-          
-          ):(<>
-            <Link to = "/signup">Signup</Link>
-          <Link to = "/login">Login</Link></>)
-          }
-        
-        </div>
-      </Toolbar>
-    </AppBar>
-  </Box>
-  )
+          <div className="nav-links">
+            {userInfo.isLoggedIn ? (
+              <>
+                <p className="login_name"> Welcome Saroj</p>
+                <Link to={`/`}>
+                <Button
+                onClick={handleLogout}
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Log Out
+              </Button>
+                
+                </Link>
+           
+                <Link to={`/profile`}>
+                  <img
+                    className="profile_img"
+                    src="https://cdn3.vectorstock.com/i/1000x1000/53/42/user-member-avatar-face-profile-icon-vector-22965342.jpg"
+                  ></img>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link to="/signup">Signup</Link>
+                <Link to="/login">Login</Link>
+              </>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
